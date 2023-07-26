8ae0e7e5ba16d8c995b237af67d82541ec973ee1
Harie Venad <harievenad@gmail.com>
2023-07-26 02:21:28 -0400

    find_avg_variance_sd helper method added

--- helper_functions.py
+++ helper_functions.py
@@ -365,3 +365,20 @@ def train_step_with_data_loader(model: torch.nn.Module,
     train_loss /= len(data_loader)
     train_acc /= len(data_loader)
     print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def find_avg_variance_sd(input=None):
+    """
+    Pass the list to find the mean, variance and standard deviation
+    """
+    avg = sum(input) / len(input)
+    sd_list = []
+    for i in iter(input):
+        mean = (i - int(avg))**2
+        sd_list.append(mean)
+
+    if len(sd_list) > 0:
+        variance = sum(sd_list) / (len(sd_list) - 1)
+        sd = round(variance**(1/2), 2)
+        return {"mean": avg,
+                "variance": variance,
+                "sd": sd}
